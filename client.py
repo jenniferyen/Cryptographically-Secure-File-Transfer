8362a8a47241e6870aba025309e2a0592a86cabe
jenyen <jenyen@seas.upenn.edu>
2020-04-30 23:21:32 -0400

    OOP, halfway thru client side login

--- client.py
+++ client.py
@@ -5,68 +5,109 @@ import time
 from netinterface import network_interface
 from Crypto import Random
 from Crypto.PublicKey import RSA
-from Crypto.Cipher import AES
+from Crypto.Cipher import AES, PKCS1_OAEP
 
-session_key = ''
-public_key = ''
+class Client:
 
-newUser = False
-username = ''
-password = ''
+    username = ''
+    password = ''
 
-# ---------- LOGIN PROTOCOL ---------- #
+    def __init__(self, client, server, network):
+        if client == None:
+            print(os.getcwd())
+        # if server == None:
+        #     # 
+        # if network == None:
+        #     # 
 
-
-def generate_session_key():
-    session_key = Random.get_random_bytes(32)
-    return session_key
-
-
-def load_public_key():
-    with open('test_keys/server_public.pem', 'rb') as f:
-        pubkeystr = f.read()
-    try:
-        return RSA.import_key(pubkeystr)
-    except:
-        print('Error: cannot import public key from file ')
-        sys.exit(1)
+        self.nonce = 0
+        self.session_key = ''
+        self.server_public_key = ''
 
 
-def initialize_nonce():
-    randomBytes = get_random_bytes(8)
-    counter = 0
-    return randomBytes + counter.to_bytes(8, 'big')
+    # ---------- LOGIN PROTOCOL ---------- #
 
 
-def encrypt_message(plaintext):
-    return ''
+    def generate_session_key(self):
+        session_key = Random.get_random_bytes(32)
+        return session_key
 
 
-def send_login_message():
-    print('Sending login message...')
+    def load_public_key(self):
+        with open('test_keys/server_public.pem', 'rb') as f:
+            pubkeystr = f.read()
+        try:
+            return RSA.import_key(pubkeystr)
+        except:
+            print('Error: cannot import public key from file ')
+            sys.exit(1)
 
-    login_type = 'login'
-    if newUser:
-        login_type = 'new_user'
 
-    plaintext = login_type.encode(
-        'utf-8') + username.encode('utf-8') + ':'.encode('utf-8') + password.encode('utf-8')
-    nonce = initialize_nonce()
+    def initialize_nonce(self):
+        randomBytes = Random.get_random_bytes(8)
+        counter = 0
+        return randomBytes + counter.to_bytes(8, 'big')
 
-    encrypted_msg = encrypt_message(plaintext)
 
-    print('Session is successfully established.')
+    def increment_nonce(self):
+        nonce = nonce[:8] + (int.from_bytes(nonce[8:], 'big') + 1).to_bytes(8, 'big')
+
+
+    def encrypt_message(self, plaintext, data=b''):
+        # if string, encode as bytes
+        if isinstance(plaintext, str):
+            plaintext = plaintext.encode('utf-8')
+
+        # initalize cipher for AES
+        cipher_aes = AES.new(session_key, AES.MODE_GCM, nonce=nonce)
+        ciphertext, tag = cipher_aes.encrypt_and_digest(plaintext)
+
+        # if there is data, encrypt it along with the original plaintext, ow just the plaintext
+        if data != b'':
+            ciphertext, tag = cipher_aes.encrypt_and_digest(plaintext + ' '.encode('utf-8') + data)
+        else:
+            ciphertext, tag = cipher_aes.encrypt_and_digest(plaintext)
+
+        increment_nonce(nonce)
+        return ciphertext, tag
+
+
+    def initialize_login(self):
+        print('Sending login message...')
+
+        self.nonce = initialize_nonce()
+        self.session_key = generate_session_key()
+        self.server_public_key = load_public_key()
+
+        login_type = 'login'
+        if new_user:
+            login_type = 'new_user'
+
+        plaintext = login_type.encode('utf-8') + username.encode('utf-8') + ':'.encode('utf-8') + password.encode('utf-8')
+        encrypted_msg = encrypt_message(plaintext)
+
+        cipher_rsa = PKCS1_OAEP.new(server_public_key)
+
+        print('Session is successfully established.')
+        return 
 
 
 # ---------- MAIN ---------- #
 
-print('Beginning main routine...')
+def main(new_user, client, server, network):
+    print('Beginning main routine...')
+    curr_client = Client(client, server, network)
 
 try:
     opts, args = getopt.getopt(sys.argv[1:], shortopts='hu:p:', longopts=['help', 'username=', 'password='])
 except:
     print('Usage: ')
     print('client.py -u <username> -p <password>')
+
+new_user = False
+client = None
+server = None
+network = None
 
 for opt, arg in opts:
     if opt == '-h' or opt == '--help':
@@ -78,6 +119,4 @@ for opt, arg in opts:
     elif opt in ('-p', '--password'):
         password = arg
 
-# initialize keys
-session_key = generate_session_key()
-public_key = load_public_key()
+main(new_user, client, server, network)
